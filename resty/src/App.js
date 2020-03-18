f67f4d6da7a99af77cbe2a167e2b0c3d9661fa9f
AstroKD <kdreyerinseattle@gmail.com>
2020-03-18 16:19:08 -0700

    html setup

--- resty/src/App.js
+++ resty/src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Form() {
+  return (
+    <div className="Form">
+      <form>
+        <input type="text" id="apiurl" name="urlpath" value="apiurl"></input>
+        <div className="RESTmethods">
+          <span>GET</span><span>POST</span><span>PUT</span><span>DELETE</span>
+          <button>Go!</button>
+        </div>
+      </form>
+      <article className="results"></article>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>RESTy</h1>
       </header>
+      <Form />
+      <footer>
+        Codefellows JS 401d34 Lab 28 Kevin Dreyer
+      </footer>
     </div>
   );
 }
